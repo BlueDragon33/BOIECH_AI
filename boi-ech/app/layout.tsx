b38d4507b5182f43f96a412a3d93042fe58d2bbf
BlueDragon33 <dinhnam331007@gmail.com>
2026-09-17 14:06:24 +0700

    Mount learner-specific dashboard shell

--- boi-ech/app/layout.tsx
+++ boi-ech/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./student-role-shell.css";
+import StudentRoleShell from "./student-role-shell";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -46,6 +48,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <StudentRoleShell />
       </body>
     </html>
   );
