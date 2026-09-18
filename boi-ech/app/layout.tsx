7489f4f580ef5caec0a807dcbee6d2662eea0535
BlueDragon33 <dinhnam331007@gmail.com>
2026-09-18 14:28:22 +0700

    Load L9 two-way messaging styles

--- boi-ech/app/layout.tsx
+++ boi-ech/app/layout.tsx
@@ -17,6 +17,7 @@ import "./teacher-dashboard-actions-v7.css";
 import "./teacher-dashboard-visual-v8.css";
 import "./teacher-learning-analytics-l7.css";
 import "./teacher-smart-interventions-l8.css";
+import "./two-way-messaging-l9.css";
 import StudentRoleShell from "./student-role-shell";
 import StudentTeacherInbox from "./student-teacher-inbox";
 import StudentAdaptiveCoach from "./student-adaptive-coach";
